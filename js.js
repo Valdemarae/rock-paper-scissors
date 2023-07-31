7d67773d5295250df9a0631ed33e27e68de6946f
Valdemar <valdemar.kobelis@gmail.com>
2023-07-31 11:12:46 +0300

    Add function in js to play a round

--- js.js
+++ js.js
@@ -5,6 +5,17 @@ function getComputerChoice () {
         case 1:
             return 'Paper';
         case 2:
-            return 'scissors';
+            return 'Scissors';
     }
 }
+function singleRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection == computerSelection) {
+        console.log(`Tie! You both got ${playerSelection}!`)
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors')) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+    }
+}
+singleRound('paper', getComputerChoice());

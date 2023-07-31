0b48f506dbeb652a372aa3f44d96ca9057d1a9dc
Valdemar <valdemar.kobelis@gmail.com>
2023-07-31 13:21:52 +0300

    Add loop to prompt for input until it is valid

--- js.js
+++ js.js
@@ -9,7 +9,6 @@ function getComputerChoice () {
     }
 }
 function singleRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection == computerSelection) {
         return `tie`;
     } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Rock')) {
@@ -23,6 +22,11 @@ function game () {
     let computerScore = 0;
     for (let i = 1; i <= 5; i++) {
         let playerSelection = prompt("Enter Rock, Paper or Scissors!");
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        while (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection != 'Scissors') {
+            playerSelection = prompt("WRONG INPUT! Enter Rock, Paper or Scissors!");
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        }
         let computerSelection = getComputerChoice();
         let result = singleRound(playerSelection, computerSelection);
         if (result == 'tie') {
@@ -36,11 +40,11 @@ function game () {
         }
     }
     if (playerScore > computerScore) {
-        console.log("You won the game! Congratulations!");
+        console.log("You won the game with score " + playerScore + " : " + computerScore + "! Congratulations!");
     } else if (computerScore > playerScore) {
-        console.log("You lost the game!"); 
+        console.log("You lost the game with score " + playerScore + " : " + computerScore + "!"); 
     } else {
-        console.log("No one won the game! It is a tie!");
+        console.log("No one won the game! It is a tie! Score is " + playerScore + " : " + computerScore + ".");
     }
 }
 game();
